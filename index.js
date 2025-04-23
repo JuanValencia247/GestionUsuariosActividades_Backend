d32bde87f3a7aac4959be6bfe40872263a48cf9b
unknown <aneuidi@gmail.com>
2025-04-23 17:18:03 -0500

    Remover node_modules del repositorio y agregadp al  .gitignore

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./config/db');
+require('dotenv').config();
+
 
 const app = express();
 connectDB();
